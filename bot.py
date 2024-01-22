05ff21db36340990fe4a18ecd479db59e0f51a38
yedekho <143316647+yedekho@users.noreply.github.com>
2024-01-22 13:04:52 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,9 @@
 import requests
+import zipfile
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
-import random
+import random 
 
 # Download ChromeDriver
 url = "https://chromedriver.storage.googleapis.com/107.0.5304.62/chromedriver_linux64.zip"
@@ -11,13 +12,14 @@ response = requests.get(url)
 with open('chromedriver.zip', 'wb') as f:
   f.write(response.content)
 
-!unzip chromedriver.zip
-!chmod +x chromedriver
+# Unzip ChromeDriver 
+with zipfile.ZipFile("chromedriver.zip","r") as zip_ref:
+    zip_ref.extractall()
 
 # Set ChromeDriver location
 chrome_options = webdriver.ChromeOptions()
-chrome_options.headless = True 
-chrome_options.binary_location = "./chromedriver"
+chrome_options.headless = True
+chrome_options.binary_location = "./chromedriver"  
 
 driver = webdriver.Chrome(options=chrome_options)
 
